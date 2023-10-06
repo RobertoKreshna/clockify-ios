fe9553cf8dd1b52ea53f4c3c8528fd0144f790ee
RobertoKreshna <roberto.kreshnain@gmail.com>
2023-10-06 10:27:24 +0700

    Initial Commit

---
+++ clockify-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clockify-ios
+//
+//  Created by Roberto Kreshna on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
